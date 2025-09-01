b9a88118e5e65ed622964a70c5ca32134f3ccabd
Your Name <your-email@example.com>
2025-09-02 01:40:56 +0530

    backend workmate

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL;
-export const WS_URL = import.meta.env.VITE_WS_URL;
+
+export const API_URL = "https://workmate-backend-1.onrender.com";
+export const WS_URL = "wss://workmate-backend-1.onrender.com";
+
